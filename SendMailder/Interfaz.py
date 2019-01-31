8b1ad5b38aec692c80dcd894d1bf351818c7d981
RGiskard <rcondori@gmail.com>
2019-01-30 21:23:31 -0500

    add windows parameters

--- SendMailder/Interfaz.py
+++ SendMailder/Interfaz.py
@@ -1,9 +1,17 @@
 from tkinter import *
+import platform
+plataform=platform.system()
 raiz=Tk()
 raiz.title("Mailder")
 raiz.resizable(0,0)
 raiz.geometry("500x500+300+300")
-raiz.iconbitmap("mail.ico")
+if plataform=="Windows":
+	raiz.iconbitmap("mail.ico")
 miFrame=Frame()
 miFrame.pack()
+
 raiz.mainloop()
+
+
+
+
