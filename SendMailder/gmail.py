7d6c7663e7bce4690afc214acdd991642063370f
RGiskard <rcondori@gmail.com>
2019-01-30 19:25:33 -0500

    Interfaz grafica easy

--- SendMailder/gmail.py
+++ SendMailder/gmail.py
@@ -53,7 +53,18 @@ def sentMail():
 		mailServer.quit()
 
 
+from Tkinter import *
 
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.hi_there = Button(frame, text="Hello World", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+    def say_hi(self):
+        print "hola todo el mundo!"
 
- 
+root = Tk()
+app = App(root)
+root.mainloop()
 
