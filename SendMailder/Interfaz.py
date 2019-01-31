fd1400098c664ab018f24c7ece3183e3b91e89c9
RGiskard <rcondori@gmail.com>
2019-01-31 00:50:59 -0500

    add finals variables with dict

--- SendMailder/Interfaz.py
+++ SendMailder/Interfaz.py
@@ -1,18 +1,20 @@
 from tkinter import *
 from tkinter import filedialog
 import platform
-gmailaddress=""
-gmailpassword=""
+
 plataform=platform.system()
 raiz=Tk()
 raiz.title("Mailder")
 raiz.resizable(0,0)
-raiz.geometry("500x500+300+300")
+raiz.geometry("300x300+300+300")
 if plataform=="Windows":
 	raiz.iconbitmap("mail.ico")
 #miFrame=Frame()
 #miFrame.pack()
-cuadroCorreo=Entry(raiz,textvariable=gmailaddress)
+
+Keys={}
+
+cuadroCorreo=Entry(raiz)
 cuadroCorreo.grid(row=0,column=1,padx=10,pady=10)
 
 correolabel=Label(raiz,text="Correo:  ")
@@ -30,10 +32,23 @@ passwordlabel.grid(row=1,column=0,padx=10,pady=10)
 
 def getFile():
    raiz.filename=filedialog.askopenfilename(initialdir = "/",title = "Seleccione archivos csv",filetypes = (("Archivos csv","*.csv"),("Todos los Archivos","*.*")))
-   print(raiz.filename)
+   Keys["gmailaddress"]=cuadroCorreo.get()
+   Keys["gmailpassword"]=cuadroPasword.get()
+   Keys["ruta"]=raiz.filename
+
+
+def Execute():   
+	print("ruta: "+Keys["ruta"])
+	print("gmailaddress: "+Keys["gmailaddress"])
+	print("gmailpassword: "+Keys["gmailpassword"])
+
 
 botonBuscar=Button(raiz,text="Buscar csv",command = getFile)
 botonBuscar.grid(row=2,column=1)
+
+botonBuscar=Button(raiz,text="Enviar Correos",command=Execute)
+botonBuscar.grid(row=3,column=1)
+
 raiz.mainloop()
 
 
