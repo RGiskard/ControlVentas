a8995c7bf3664c286b153223d8886c167bf8e0b9
RGiskard <rcondorib@gmail.com>
2019-01-30 21:17:11 -0500

    Add icon

--- SendMailder/Interfaz.py
+++ SendMailder/Interfaz.py
@@ -1,8 +1,9 @@
-from Tkinter import *
+from tkinter import *
 raiz=Tk()
 raiz.title("Mailder")
 raiz.resizable(0,0)
-raiz.geometry("500x500+300+300") 
+raiz.geometry("500x500+300+300")
+raiz.iconbitmap("mail.ico")
 miFrame=Frame()
 miFrame.pack()
 raiz.mainloop()
